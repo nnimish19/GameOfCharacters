e96bee268130b0d6c41d112b41265cd979146768
nnimish19 <nnimish19@gmail.com>
2016-12-13 20:55:27 -0500

    log program run time

--- Alexa/Series Trivia.py
+++ Alexa/Series Trivia.py
@@ -10,6 +10,7 @@ http://amzn.to/1LGWsLG
 from __future__ import print_function
 import re
 from Queue import *
+import time
 
 EntityStartSeq = "<<<<<"
 AttributeStartSeq = "<<<"
@@ -976,6 +977,9 @@ def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
+    start = time.time()
+    response = {}
+    
     print("event.session.application.applicationId=" +
           event['session']['application']['applicationId'])
 
@@ -993,9 +997,13 @@ def lambda_handler(event, context):
                            event['session'])
 
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        response = on_launch(event['request'], event['session'])
     elif event['request']['type'] == "IntentRequest":
-        return on_intent(event['request'], event['session'])
+        response = on_intent(event['request'], event['session'])
     elif event['request']['type'] == "SessionEndedRequest":
-        return on_session_ended(event['request'], event['session'])
+        response = on_session_ended(event['request'], event['session'])
+
+    stop = time.time()
+    print ("Total Time(sec): ", start-stop)
+    return response
     
